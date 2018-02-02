b3f639488752a9a1918c30ae823ffe094b64d9e3
Vincent Ho <doiphongba@gmail.com>
2018-02-02 13:18:02 -0500

    Implemented destroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -43,6 +43,14 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
+    end
+  end
+
   private
    def portfolio_params
      params[:portfolio].permit(:title, :subtitle, :body)
